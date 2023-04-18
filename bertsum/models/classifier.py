4db6fef773a91cd2cd00a896add2a0aedc967c11
James <nguyenthecoder@gmail.com>
2023-04-18 10:58:00 -0400

    working on baseline models

---
+++ bertsum/models/classifier.py
@@ -0,0 +1,15 @@
+# import torch
+import torch.nn as nn
+# from torch.nn.init import xavier_uniform_
+
+class Classifier(nn.Module):
+
+    def __init__(self, num_hiddens):
+        super(Classifier, self).__init__()
+        self.linear1 = nn.Linear(num_hiddens, 1)
+        self.sigmoid = nn.Sigmoid()
+    
+    def forward(self, x, mask_cls):
+        h = self.linear1(x).squeeze(-1)
+        sent_score = self.sigmoid(h) * mask_cls.float()
+        return sent_score
